852aab754d328767771db20f2a01dbdb1795e7b1
Ubuntu <ubuntu@ip-172-31-0-7.eu-west-1.compute.internal>
2013-07-08 08:54:20 +0000

    Modified web.js to use filesync and buffer

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var file = "index.html";
+fs.writeFileSync(file, result);
+var buffer = new Buffer(result, "utf-8");
+var hello = buffer.toString("utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
