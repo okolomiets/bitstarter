34aa8e6558ed0ff37e006e259745f1c1b6b7ecdd
Ubuntu <ubuntu@ip-172-31-0-7.eu-west-1.compute.internal>
2013-07-08 09:31:40 +0000

    Fixed web.js to populate data from file

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-fs.readFileSync('index.html');
-//var content = buffer.toString("utf-8");
+var buffer = fs.readFileSync('index.html');
+var content = buffer.toString("utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
